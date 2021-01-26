f74fcd8411095cdddff8631c6427caac95b46456
Kenbot101 <johnkenneth201@gmail.com>
2021-01-26 20:53:11 +0100

    added Info

--- app/models.py
+++ app/models.py
@@ -15,3 +15,31 @@ class User(db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.name)
 
+class Info(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    DOB = db.Column(db.String(20))
+    nationality = db.Column(db.String(120))
+    POB = db.Column(db.String(120))
+    POO = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    M_S = db.Column(db.String(120))
+    religion = db.Column(db.String(120))
+    address = db.Column(db.String(200))
+    kin = db.Column(db.String(120))
+    kin_address = db.Column(db.String(200))
+    previous_uni = db.Column(db.String(120))
+    program = db.Column(db.String(120))
+    mode = db.Column(db.String(120))
+    qualification = db.Column(db.String(120))
+    award = db.Column(db.String(120))
+    study = db.Column(db.String(120))
+    faculty = db.Column(db.String(120))
+    department = db.Column(db.String(120))
+    duration = db.Column(db.String(120))
+    health = db.Column(db.String(120))
+    kin_relationship = db.Column(db.String(120))
+    kin_no = db.Column(db.String(120))
+    user_id = db.Column(db.Integer,index=True,unique=True)
+
+
+
